3aeb73b312c026eb37f092d577f5d7bfa2edf4ac
Zdravko Dimitrov <zdravko@MacBook-Pro-de-Zdravko-2.local>
2021-04-01 20:24:49 +0200

    actualización código

--- pract03/src/main/java/controlador/AlarmasControlador.java
+++ pract03/src/main/java/controlador/AlarmasControlador.java
@@ -1,8 +1,20 @@
 package controlador;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import modelo.Alarma;
 import modelo.Alarmas;
 import vista.MainWindow;
 
+/**
+ * El controlador coordina operaciones entre la vista y el modelo.
+ * @author Zdravko y Sara
+ *
+ */
 public class AlarmasControlador {
 	
 	private Alarmas modelo;
@@ -12,6 +24,40 @@ public class AlarmasControlador {
 		modelo = m;
 		vista = v;
 		
+		/*Indica que cuando se pulse el botón de añadir alarma,
+		 * se ejecutará la operación desde la clase interna NuevaAlarmaListener.*/
+		vista.addAnhadeAlarmaListener(new NuevaAlarmaListener());
+		
+	}
+	
+	public class NuevaAlarmaListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			
+			String nombreAlarma  = "";
+			nombreAlarma = vista.getNombreAlarma();
+			
+			SimpleDateFormat sdf = new SimpleDateFormat ("yyyy-MM-dd");
+			Date fechaAlarma = null;
+			try {
+				fechaAlarma = sdf.parse("2020-04-3");
+			} catch (ParseException e) {
+				System.out.println("Se ha detectado una excepción.");
+				e.printStackTrace();
+			}
+			
+			Alarma alarma = new Alarma(nombreAlarma, fechaAlarma);
+			vista.MuestraInformaciónAlarma(alarma);
+			
+			
+			
+		}
+		
+		
+		
+		
+		}	
 	}
 
-}
+
--- pract03/src/main/java/vista/MainWindow.java
+++ pract03/src/main/java/vista/MainWindow.java
@@ -2,6 +2,8 @@ package vista;
 
 import java.awt.EventQueue;
 
+import java.awt.event.ActionListener;
+import java.awt.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
@@ -9,7 +11,9 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
 
+import controlador.AlarmasControlador;
 import modelo.Alarma;
+import modelo.Alarmas;
 
 import javax.swing.JButton;
 import javax.swing.JTextArea;
@@ -17,35 +21,45 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JSpinner;
 
-// TOOD: MOVER A CLASE A PARTE?
-public interface SelectionListener {
-	public void widgetSelected(SelectionEvent ev); // TODO error selectionEvent?
-}
 
-public abstract class Widget {
-	public void addSelectionListener(SelectionListener sl) {}
-}
+//importaciones
+import java.awt.event.ActionListener;
 
 public class MainWindow {
+	
+	
 
-	private JFrame frame;
+	JFrame frame;
 	private JTextField txtNombreAlarma;
+	
+	//declaración elementos
+	private JButton btnAnhadir;
+	private JButton btnOn;
+	private JButton btnOff;
+	private JButton btnEliminar;
 
+	
 	/**
 	 * Launch the application.
 	 */
+	/*
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MainWindow window = new MainWindow();
-					window.frame.setVisible(true);
+					
+					MainWindow vista = new MainWindow();
+					Alarmas modelo = new Alarmas();
+					AlarmasControlador controlador = new AlarmasControlador(modelo, vista);
+					vista.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
+	
+	*/
 
 	/**
 	 * Create the application.
@@ -90,17 +104,13 @@ public class MainWindow {
 		spinnerHora.setBounds(97, 126, 107, 20);
 		panel.add(spinnerHora);
 		
-		JButton btnAnhadir = new JButton("A\u00F1adir");
-		btnAnhadir.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				String nombre = txtNombreAlarma.getText();
-				//String hora = spinnerHora.toString();
-				
-			}
-		});
+		btnAnhadir = new JButton("A\u00F1adir");
 		btnAnhadir.setBounds(98, 216, 89, 23);
 		panel.add(btnAnhadir);
+		
+		
+		
+		
 		
 		// Seccion derecha - modificar alarma existente
 		JLabel lblModificarAlarma = new JLabel("Modificar alarma");
@@ -117,12 +127,7 @@ public class MainWindow {
 		txtAreaAlarmasAct.setBounds(295, 106, 133, 99);
 		panel.add(txtAreaAlarmasAct);
 		
-		JButton btnOn = new JButton("ON");
-		btnOn.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-			}
-		});
+		btnOn = new JButton("ON");
 		btnOn.setBounds(503, 216, 60, 23);
 		panel.add(btnOn);
 		
@@ -135,24 +140,44 @@ public class MainWindow {
 		txtAreaAlarmasDesact.setBounds(462, 106, 133, 99);
 		panel.add(txtAreaAlarmasDesact);
 		
-		JButton btnOff = new JButton("OFF");
-		btnOff.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				
-			}
-		});
+		btnOff = new JButton("OFF");
 		btnOff.setBounds(335, 216, 60, 23);
 		panel.add(btnOff);
 		
-		JButton btnEliminar = new JButton("Eliminar");
-		btnEliminar.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-			}
-		});
+		btnEliminar = new JButton("Eliminar");
 		btnEliminar.setBounds(401, 260, 89, 23);
 		panel.add(btnEliminar);
 		
+		
+		
+		
 	}
+		
+	
+	
+	public void addAnhadeAlarmaListener (ActionListener listenForNewAlarm) {
+		btnAnhadir.addActionListener(listenForNewAlarm);
+	}
+	
+	public String getNombreAlarma() {
+		return txtNombreAlarma.getText();
+	}
+	
+	public void MuestraInformaciónAlarma (Alarma alarma) {
+		System.out.println ("Nombre: "+alarma.getId()+"; Hora programada: "+alarma.getHora());
+	}
+
+	public void setVisible() {
+		frame.setVisible(true);
+		
+	}
+
+	
+	
+	
+	
+	
 }
+	
+	
+
