a25e99e3c47c7fef91e7d58ef72ab6722dd7d1c3
SaraGrela <sgc114@alumnos.unican.es>
2021-04-21 12:08:41 +0200

    Implementada clase Test y métodos básicos para las clases Seguro y Cliente.

--- IS_2021/Pract04/src/main/java/es/unican/is2/Seguro.java
+++ IS_2021/Pract04/src/main/java/es/unican/is2/Seguro.java
@@ -10,14 +10,76 @@ public class Seguro {
 	private Cobertura cobertura;
 	private Cliente tomadorSeguro;
 	
+	// Precio base del seguro por tipo de cobertura
+	private final static int PRECIOTODORIESGO = 1000;
+	private final static int PRECIOTERCEROSLUNAS = 600;
+	private final static int PRECIOTERCEROS = 400;
+	
+	// Excepcion
+	@SuppressWarnings("serial")
+	public static class DatoIncorrectoException extends RuntimeException {};
+	
 	public Seguro (int potencia, Cliente cliente, Cobertura cobertura) {
 		this.potenciaCV = potencia;
 		this.tomadorSeguro = cliente;
 		this.cobertura = cobertura;
 	}
 	
-	public double precio() {
-		return 0;
+	public double precio() throws DatoIncorrectoException {
+		
+		double precioBase = 0.0;
+		double porcentajePotencia = 0.0;
+		
+		if (potenciaCV < 0 || fechaUltimoSiniestro.isAfter(LocalDate.now())) { // TODO fechaIncorrecta
+			throw new DatoIncorrectoException();
+		}
+		// Cobertura
+		if (cobertura.equals(Cobertura.TODORIESGO)) {
+			precioBase = PRECIOTODORIESGO;
+		} else if (cobertura.equals(Cobertura.TERCEROSLUNAS)) {
+			precioBase = PRECIOTERCEROSLUNAS;
+		} else {
+			precioBase = PRECIOTERCEROS;
+		}
+		// Potencia
+		if (potenciaCV >= 90 && potenciaCV <= 110) {
+			porcentajePotencia = 0.05;
+		} else if (potenciaCV > 110) {
+			porcentajePotencia = 0.2;
+		} 
+		// Fecha ultimo siniestro
+		if (fechaUltimoSiniestro.isAfter(LocalDate.now().minusYears(1)) && fechaUltimoSiniestro.isBefore(LocalDate.now())) {
+			precioBase += 200;
+		} else if (fechaUltimoSiniestro.isAfter(LocalDate.now().minusYears(3)) && 
+				fechaUltimoSiniestro.isBefore(LocalDate.now().minusYears(1))) {
+			precioBase += 50;
+		}
+		// Minusvalia
+		double precio = precioBase + precioBase*porcentajePotencia;
+		if (tomadorSeguro.getMinusvalia()) {
+			return precio - precio*0.25;
+		}
+		return precio;
+	}
+	
+	public int getPotencia() {
+		return potenciaCV;
+	}
+	
+	public Cliente getCliente() {
+		return tomadorSeguro;
+	}
+	
+	public Cobertura getCobertura() {
+		return cobertura;
+	}
+	
+	public void setFechaUltimoSiniestro(LocalDate fecha) {
+		this.fechaUltimoSiniestro = fecha;
+	}
+	
+	public LocalDate getFechaUltimoSiniestro() {
+		return fechaUltimoSiniestro;
 	}
 
 }
--- IS_2021/Pract04/src/test/java/es/unican/is2/SeguroTest.java
+++ IS_2021/Pract04/src/test/java/es/unican/is2/SeguroTest.java
@@ -1,12 +1,19 @@
 package es.unican.is2;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.time.LocalDate;
+import java.time.Period;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import es.unican.is2.Seguro.DatoIncorrectoException;
+
 public class SeguroTest {
 	
-	private Seguro seguro;
+	private Seguro sutSeg;
 	private Cliente clienteMinus;
 	private Cliente clienteNoMinus;
 	
@@ -14,12 +21,103 @@ public class SeguroTest {
 	public void setUp() throws Exception {
 		clienteMinus = new Cliente("Pepe", "12345678A", true);
 		clienteNoMinus = new Cliente("Pepe", "12345678A", false);
-		//seguro = new Seguro(90, cliente, Seguro.Cobertura.TODORIESGO);
 	}
 	
 	@Test
-	public void testConstructor() {
-		//assertTrue(seguro.precio()==0);
+	public void testConstructor() { // TODO bien?
+		sutSeg = new Seguro(90, clienteMinus, Seguro.Cobertura.TODORIESGO);
+		assertTrue(sutSeg.getPotencia()==90);
+		assertTrue(sutSeg.getCliente().equals(clienteMinus));
+		assertTrue(sutSeg.getCobertura()==Seguro.Cobertura.TODORIESGO);		
 	}
-
+	
+	@Test
+	public void testPrecio() {
+		
+		// Casos de prueba validos
+		try {
+			sutSeg = new Seguro(90, clienteMinus, Seguro.Cobertura.TODORIESGO);
+			LocalDate fecha = LocalDate.now();
+			sutSeg.setFechaUltimoSiniestro(fecha.minusYears(1));
+			assertTrue(sutSeg.precio()==937.5);
+		} catch (DatoIncorrectoException e) {
+			fail();
+		}
+		
+		try {
+			sutSeg = new Seguro(100, clienteNoMinus, Seguro.Cobertura.TERCEROSLUNAS);
+			LocalDate fecha = LocalDate.now();
+			sutSeg.setFechaUltimoSiniestro(fecha.minusMonths(6));
+			assertTrue(sutSeg.precio()==830);
+		} catch (DatoIncorrectoException e) {
+			fail();
+		}
+		
+		try {
+			sutSeg = new Seguro(110, clienteMinus, Seguro.Cobertura.TERCEROS);
+			LocalDate fecha = LocalDate.now();
+			sutSeg.setFechaUltimoSiniestro(fecha);
+			assertTrue(sutSeg.precio()==465);
+		} catch (DatoIncorrectoException e) {
+			fail();
+		}
+		
+		try {
+			sutSeg = new Seguro(200, clienteNoMinus, Seguro.Cobertura.TODORIESGO);
+			LocalDate fecha = LocalDate.now();
+			sutSeg.setFechaUltimoSiniestro(fecha.minusYears(3));
+			assertTrue(sutSeg.precio()==1250);
+		} catch (DatoIncorrectoException e) {
+			fail();
+		}
+		
+		try {
+			sutSeg = new Seguro(200, clienteNoMinus, Seguro.Cobertura.TERCEROSLUNAS);
+			LocalDate fecha = LocalDate.now();
+			sutSeg.setFechaUltimoSiniestro(fecha.minusYears(2));
+			assertTrue(sutSeg.precio()==770);
+		} catch (DatoIncorrectoException e) {
+			fail();
+		}
+		
+		try {
+			sutSeg = new Seguro(90, clienteNoMinus, Seguro.Cobertura.TERCEROS);
+			LocalDate fecha = LocalDate.now();
+			sutSeg.setFechaUltimoSiniestro(fecha.minusYears(5));
+			assertTrue(sutSeg.precio()==420);
+		} catch (DatoIncorrectoException e) {
+			fail();
+		}
+		
+		// Casos de prueba no validos
+		/* El caso de prueba no valido con cobertura "terceros ampliados" no se puede implementar
+		 * debido al enumerado, por eso ya no lo probamos.
+		 * El caso de prueba no valido con potencia "AAA" no se puede implementar
+		 * porque Java controla que el campo sea un entero.
+		*/
+		try {
+			sutSeg = new Seguro(0, clienteNoMinus, Seguro.Cobertura.TODORIESGO);
+			LocalDate fecha = LocalDate.now();
+			sutSeg.setFechaUltimoSiniestro(fecha.minusYears(1));
+			sutSeg.precio();
+			fail();
+		} catch (DatoIncorrectoException e) {}
+		
+		try {
+			sutSeg = new Seguro(200, clienteNoMinus, Seguro.Cobertura.TERCEROS);
+			LocalDate fecha = LocalDate.of(2021, 13, 2);
+			System.out.printf(fecha.toString());
+			sutSeg.setFechaUltimoSiniestro(fecha);
+			sutSeg.precio();
+			fail();
+		} catch (DatoIncorrectoException e) {}
+		
+		try {
+			sutSeg = new Seguro(110, clienteNoMinus, Seguro.Cobertura.TODORIESGO);
+			LocalDate fecha = LocalDate.now();
+			sutSeg.setFechaUltimoSiniestro(fecha.plusDays(1));
+			sutSeg.precio();
+			fail();
+		} catch (DatoIncorrectoException e) {}
+	}
 }
